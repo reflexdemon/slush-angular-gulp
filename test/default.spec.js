d157985b7c4e66867d3321b4ffa01631b35dfdf0
reflexdemon <reflexdemon@users.noreply.github.com>
2015-07-19 10:01:20 -0400

    Unit testing for Route added

--- test/default.spec.js
+++ test/default.spec.js
@@ -416,6 +416,28 @@
             });
 
         }); // provider test
+        describe('route generator test', function() {
+            beforeEach(function() {
+                testingUtil.mockPrompt({
+                    module: 'module1',
+                    fileName: 'myroute'
+                });
+                util.setRuntimeMode('TEST');
+            });
+            it('should put the route file in the correct directory', function(done) {
+                gulp.start('route').once('stop', function() {
+                    assert.that(mockGulpDest.basePath()).is.endingWith('src/app/components/module1');
+                    done();
+                });
+            });
+            it('should put the correct route filename', function(done) {
+                gulp.start('route').once('stop', function() {
+                    mockGulpDest.assertDestContains('myroute-route.js');
+                    done();
+                });
+            });
+
+        }); // route test
     });
 
 })();
