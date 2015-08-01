145c50f26aaef7916a33e36bc1cc84220aa46e7c
reflexdemon <reflexdemon@users.noreply.github.com>
2015-08-01 17:10:53 -0400

    Logaccly completed half of the testcases 26/55

---
+++ test/cases/factory.spec.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    var common = require("../common");
+    var options = common.options;
+    var assert = common.assert;
+    var testingUtil = common.testingUtil;
+    var mockGulpDest = common.mockGulpDest;
+    var gulp = common.gulp;
+    var util = common.util;
+
+    function beforeEach() {
+        process.chdir(__dirname);
+        testingUtil.mockPrompt({
+            module: 'module1',
+            fileName: 'myfactory'
+        });
+        util.setRuntimeMode('TEST');
+    }
+
+    it('should put the factory file in the correct directory', function(done) {
+        beforeEach();
+        gulp.start('factory').once('stop', function() {
+            assert.that(mockGulpDest.basePath()).is.endingWith('src/app/components/module1');
+            done();
+        });
+    });
+    it('should put the correct factory filename', function(done) {
+        beforeEach();
+        gulp.start('factory').once('stop', function() {
+            mockGulpDest.assertDestContains('myfactory-factory.js');
+            done();
+        });
+    });
+
+})();
